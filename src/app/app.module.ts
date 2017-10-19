44ccde3328483ebd0746de4f377165ddf0603b45
moosubb4 <rollingpork@gmail.com>
2017-10-19 20:33:32 +0700

    builddd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,8 @@ import { FireCloundService } from "./services/fire-clound.service";
 // FireStore
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
-// import { environment } from "../environments/environment.prod";
-import { environment } from "../environments/environment";
+import { environment } from "../environments/environment.prod";
+// import { environment } from "../environments/environment";
 
 const appRoutes: Routes = [
   { path: "", component: FireShowComponent },
